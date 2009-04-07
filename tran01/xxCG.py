530c0921f158ca8d8716567bef49820a02a3856c
dave <dave@sabine.akapt.mooo.com>
2009-04-07 12:32:16 -0700

    Bop: xxCG works, but needs a nice long run with lots of iterations to produce a clean image

--- tran01/xxCG.py
+++ tran01/xxCG.py
@@ -28,10 +28,43 @@ class xxCG():
         # Diffusion related values
         self.T = 288             #the temperature (in K) that this runs at
         self.K = 1.381 * 10**-23 #Boltzman const (in J/K)
-        self.kT = self.K * self.T * 10**23 # kT with pN/nM conversion
+        self.kT = self.K * self.T * 10**21 # kT with pN/nM conversion
         self.Gz = sqrt((pi * self.kT) / (2 * self.Gk))
         self.Cz = sqrt((2 * pi * self.kT) / self.Ck)
-
+    
+    def bop(self):
+        """Bop the xb to a new location, based on an exponential distribution 
+        of energies for each independent segment of the crossbridge as 
+        determined by Boltzmann's law. Return the new head location.
+        
+        This makes use of numpy's exponential distribution:
+            numpy.random.exponential(scale=1.0, size=None)
+        Where scale is B such that the probability density function of the 
+        resulting distribution is: 
+            f(x, B) = 1/B exp(-x/B)
+        If we feed numpy's exponential distribution B=kT, and take x as 
+        energy U, we have a distribution of 
+            f(U) = 1/kT exp(-U/kT)
+        Now, we will have to scale this output as we need our distributions 
+        to have probability density functions of:
+            f(U) = 1/Z exp(-U/kT) 
+                Where Z is in the format of:
+                Zc = sqrt(2 pi kT / Kc) or
+                Zg = sqrt(pi kT / (2 Kg))
+        This means we need to multiply the numpy output by kT/Z, which, in 
+        these cases would work out to:
+            kT/Zc = sqrt(Kc kT/ (2 pi)) or
+            kT/Zg = sqrt(2 Kg kT / pi)
+        This gives us the energy distributions we want, from which we can 
+        backtrack to get the length or angle values for each component.
+        """
+        Cu = self.kT / self.Cz * np.random.exponential(scale=self.kT)
+        C  = sqrt(2 * Cu / self.Ck) + self.Cs
+        Gu = self.kT / self.Gz * np.random.exponential(scale=self.kT)
+        G  = sqrt(2 * Gu / self.Gk) + self.Gs
+        self.set_conv_and_head_from_segments(C, G)
+        return self.head_loc
+        
     def probability(self):
         """Given the location of the XB head,
         return the probability that it is there,
@@ -64,30 +97,35 @@ class xxCG():
         """Set the head loc to its rest location"""
         self.head_loc = (self.Gs * cos(self.Cs),
                          self.Gs * sin(self.Cs))
+        
+    def set_conv_and_head_from_segments(self, C, G):
+        """Set the converter and head loc from passed segment values"""
+        self.head_loc = (G * cos(C),
+                         G * sin(C))
+
     
 
 ## Begin the script that will produce the matrix of stored probabilities
-x_locs = np.arange(-3, 13, .2) 
-y_locs = np.arange(0, 16, .2)
+trials = 96000
+x_locs = np.arange(-15, 15, .2) 
+y_locs = np.arange(-5, 25, .2)
 probs = np.zeros((y_locs.size, x_locs.size))
+hits = np.zeros((y_locs.size, x_locs.size))
 # Instantiate the xb
 xb = xxCG()
-# Cycle through and collect all the probabilities
-n = [0,0]
-for y in y_locs:
-    for x in x_locs:
-        xb.head_loc = (x,y)
-        probs[n[0], n[1]] = xb.probability()
-        n[1] = n[1] + 1
-    n[0] = n[0] + 1
-    n[1] = 0
-# Normalize the probabilities
-min = np.min(probs)
-max = np.max(probs)
-probs = (probs - min)/(max-min)
+# Cycle through all iterations and collect the head locations
+for i in range(trials):
+    loc = xb.bop()
+    x_ind = np.searchsorted(x_locs, loc[0]) - 1 #FIXME Check that there is not  
+    y_ind = np.searchsorted(y_locs, loc[1]) - 1 #    an off by one error here
+    hits[x_ind, y_ind] = hits[x_ind, y_ind] + 1
+# Normalize the hit likelihood
+min = np.min(hits)
+max = np.max(hits)
+hits = (hits - min)/(max-min)
 contour.title = "Probability of an xxCG crossbridge being\n found at a given head locations"
 contour.xlabel = "Location of XB head (nm)"
 contour.ylabel = "Location of XB head (nm)"
-contour.levels = [.9, .95, .98, .99, .999] 
-contour.contour(x_locs, y_locs, probs)
-print(probs)
+contour.levels = [.1, .3, .5, .7, .9] 
+contour.contour(x_locs, y_locs, hits)
+print(hits)
