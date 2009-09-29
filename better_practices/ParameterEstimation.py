579634a0acfc3d8003f8ad68c13b20ffb283e96a
Dave Williams <cdave@u.washington.edu>
2009-09-28 17:40:08 -0700

    Lattice spacing calculations now in ParameterEstimation

--- better_practices/ParameterEstimation.py
+++ better_practices/ParameterEstimation.py
@@ -6,19 +6,12 @@ Created by Dave Williams on 2009-09-09.
 Copyright (c) 2009 Dave Williams. All rights reserved.
 """
 
-import sys
-import os
-import numpy as np
+
 from numpy import pi, degrees, radians, cos, sin, hypot, arctan2
 
-def head_and_conv_locs(xb, state):
-    xb = xb[state]
-    conv_loc = (xb['neck_length'] * cos(xb['thic_angle']),
-                xb['neck_length'] * sin(xb['thic_angle']))
-    # need to calc the head loc, return both
-    
 
 def main():
+    """Given some 4sXB parameters, calculate 2sXB parameters and other info"""
     # Diagram of the crossbridge head for reference.
     # 
     # =( )==================   Thick filament       
@@ -36,24 +29,7 @@ def main():
     #   |              88  <-- Tip                  
     #   |                                           
     #   Angle Theta                                 
-    
-    # From Morel's 1997 work: radial forces shift from compressive to 
-    # expansive when lattice spacing drops below 34nm d10, making this a 
-    # reasonable value for the resting radial offset of our crossbridge heads.
-    #
-    # From Millman's 1998 review: The rest lattice spacing of muscle (type?)
-    # is 37nm d10.
     # 
-    # In Taylor's 1999 Cell paper (clearly explained in Davis and Epstein's
-    # 2009 paper): the angle between what Reedy refers to as the light chain
-    # domain (LCD) and the axial axis goes from 125 degrees to 70 degrees. 
-    #
-    # If we assume that the catalytic domain (CD) is remaining radially 
-    # oriented during this shift in LCD angle, this means that the angle 
-    # between the LCD and the CD goes from 215 degrees to 160 degrees
-    #
-    #
-    
     # From Liu et al's 2006 paper (which admittedly is using IFM), the most
     # frequently occurring thick fil to s2 angle range is 51-60 degrees. We 
     # assume that this range is being distorted by the compressive radial 
@@ -104,23 +80,23 @@ def main():
     
     print "Input for the 4sXB:"
     print "==================="
-    print "Theta (deg): %5.2f" %degrees(theta)
-    print "S2 domain length (nm): %5.2f" %s2_len
+    print "Theta (deg): %5.2f" % degrees(theta)
+    print "S2 domain length (nm): %5.2f" % s2_len
     print "Phi (deg), measured from S2 to the LCD: %5.2f pre-powerstroke" \
-            %degrees(phi[0])
+            % degrees(phi[0])
     print "Phi (deg), measured from S2 to the LCD: %5.2f post-powerstroke" \
-            %degrees(phi[1])
-    print "LCD length (nm): %5.2f" %lcd_len
+            % degrees(phi[1])
+    print "LCD length (nm): %5.2f" % lcd_len
     print " "
     
     print "Results for the 4sXB:"
     print "====================="
     print "S2-LCD joined at : [%5.2f, %5.2f]" \
-            %(hinge[0], hinge[1])
+            % (hinge[0], hinge[1])
     print "LCD-CD joined at : [%5.2f, %5.2f] pre-powerstroke" \
-            %(head[0][0], head[0][1])
+            % (head[0][0], head[0][1])
     print "LCD-CD joined at : [%5.2f, %5.2f] post-powerstroke" \
-            %(head[1][0], head[1][1])
+            % (head[1][0], head[1][1])
     print " "
     
     # Now to calculate the parameters for the 2 spring crossbridge we first 
@@ -152,9 +128,50 @@ def main():
     print "====================="
     print "Head locations matches 4sXB: " + str(all_match)
     print "Lin spring lengs (nm): %5.2f pre and %5.2f post" \
-            %(lin_len[0], lin_len[1])
+            % (lin_len[0], lin_len[1])
     print "Tor spring angs (deg): %5.2f pre and %5.2f post" \
-            %(degrees(tor_ang[0]), degrees(tor_ang[1]))
+            % (degrees(tor_ang[0]), degrees(tor_ang[1]))
+    print " "
+    
+    # Calculate the lattice spacing conversion formulas
+    
+    # From Morel's 1997 work: radial forces shift from compressive to 
+    # expansive when lattice spacing drops below 34nm d10, making this a 
+    # reasonable value for the resting radial offset of our power-generating 
+    # crossbridge heads.
+    #
+    # From Millman's 1998 paper, we derive that the 
+    # myosin-center-to-actin-center distance, times 1.5, yields the d10 
+    # lattice spacing that is typically used as a measure of lattice spacing. 
+    # So if we want to convert our radial coordinates to the d10 lattice 
+    # spacing, we need to find the offset that accounts for thickness of the 
+    # filaments and the CD. 
+    #
+    # Note: This is somewhat less than you would expect if you know the thick 
+    # filament diameter is 26 to 31nm (from Millman, 1998 on pg378 and Kensler 
+    # & Harris, 2008, PMCID: PMC2242758 respectively) and that the thin 
+    # filament diameter is 9.5nm (from Millman, 1998 on pg378). We attribute 
+    # this to some of the springs we are describing lying within what would 
+    # be considered the thick filament. Also, the crossbridge head may 
+    # actually be binding to the side of the thin filament, not to the face 
+    # oriented to the thick filament, so that the XB is actually reaching past 
+    # the surface of the thick filament when viewed in the radial plane.
+    
+    # d10 = 1.5 * (our_ls + offset)
+    # d10 - 1.5 * our_ls = 1.5*offset
+    # d10 / 1.5 - our_ls = offset
+    ls_offset = 34/1.5 - head[1][1]
+    
+    # Display the lattice spacing results
+    
+    print "Results for lattice spacing:"
+    print "============================"
+    print "Lattice spacing formula: d10 = 1.5 * (ls + offset)"
+    print "Offset: %5.2f" % ls_offset
+    
+    # From Millman's 1998 review: The rest lattice spacing of muscle (type?)
+    # is 37nm d10. This is also a reasonable value for the rest d10 spacing 
+    # used above and may be substituted if needed.
     
 
 if __name__ == '__main__':
